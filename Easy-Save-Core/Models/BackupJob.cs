24f9612d19f415dcec7ab221df9449c9881d19da
hungrywwaffle <78042192+hungrywwaffle@users.noreply.github.com>
2025-05-09 16:31:22 +0200

    create BackupJob and BackupJobTasks
    Create classes BackupJob and BackupJobTasks
    Start implementing File.Copy

---
+++ Easy-Save-Core/Models/BackupJob.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CLEA.EasySaveCore.Models;
+
+//TODO : parcours fichiers dans dossier source -> un job task par fichier
+
+namespace EasySaveCore.Models
+{
+    internal class BackupJob
+    {
+        public Property<string> Name;
+        public Property<DateTime> Timestamp;
+        public Property<string> Source;
+        public Property<string> Target;
+        public Property<long> Size;
+        public Property<long> TrasferTime;
+
+        protected BackupJob(string name, string source, string target)
+        {
+            Name = new Property<string>("name", name) ;
+            Timestamp = new Property<DateTime>("timestamp", 0);
+            Source = new Property<string>("source", source);
+            Target = new Property<string>("target", target);
+            Size = new Property<long>("size", 0);
+            TrasferTime = new Property<long>("transferTime", 0);
+        }
+
+        static void Main()
+        {
+            var watch = Stopwatch.StartNew();
+            BackupJob backupJob = new BackupJob("name", "source", "target");
+            BackupJobTask backupJobTask = new BackupJobTask(backupJob);
+            backupJobTask.ExecuteTask();
+            watch.Stop();
+            BackupJob timestamp = watch.ElapsedMilliseconds;
+        }
+    }
+}
---
+++ Easy-Save-Core/Models/BackupJobTask.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using CLEA.EasySaveCore.Models;
+
+namespace EasySaveCore.Models
+{
+    internal class BackupJobTask : JobTask
+    {
+        public Property<string> Name;
+        public Property<DateTime> Timestamp;
+        public Property<string> Source;
+        public Property<string> Target;
+        public Property<long> Size;
+        public Property<long> TrasferTime;
+        public BackupJob BackupJob;
+
+        public BackupJobTask(BackupJob backupJob) : base(backupJob.Name.Value)
+        {
+            this.Name = backupJob.Name;
+            this.Timestamp = backupJob.Timestamp;
+            this.Source = backupJob.Source;
+            this.Target = backupJob.Target;
+            this.Size = backupJob.Size;
+            this.TrasferTime = backupJob.TrasferTime;
+            this.BackupJob = backupJob;
+        }
+
+        public override void Deserialize(JsonObject data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ExecuteTask()
+        {
+
+            string[] SourceDirectoriesArray = Directory.GetDirectories(this.Source.Value, "*", SearchOption.AllDirectories);
+
+            foreach (string directory in SourceDirectoriesArray) {
+
+                string dirToCreate = directory.Replace(this.Source.Value, this.Target.Value);
+                Directory.CreateDirectory(dirToCreate);
+            }
+
+            string[] SourceFilesArray = Directory.GetFiles(this.Source.Value, "*.*", SearchOption.AllDirectories);
+
+            foreach (string path in SourceFilesArray)
+            {
+                File.Copy(path, path.Replace(this.Source.Value, this.Target.Value));
+            }
+
+        }
+
+        public override JsonObject Serialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
