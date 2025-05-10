69e35f6c5ec502bf0e3c00f857ed064deb8b9d0f
Enorian <enorian.rajo@gmail.com>
2025-05-10 18:17:35 +0200

    Update name property of JobTask and IJob

--- Easy-Save-Core/Models/JobTask.cs
+++ Easy-Save-Core/Models/JobTask.cs
@@ -5,6 +5,27 @@ namespace CLEA.EasySaveCore.Models;
 
 public abstract class JobTask : IJsonSerializable
 {
+    public string Name
+    {
+        get
+        {
+            Property<dynamic>? property = _properties.Find(tp => tp.Name == "name");
+
+            if (property == null)
+                throw new Exception("Name property not found");
+            
+            return property.Value;
+        }
+        set
+        {
+            Property<dynamic>? property = _properties.Find(tp => tp.Name == "name");
+
+            if (property == null)
+                throw new Exception("Name property not found");
+            
+            property.Value = value;
+        }
+    }
     private readonly List<Property<dynamic>> _properties = new List<Property<dynamic>>();
     
     protected JobTask(string name)
@@ -12,19 +33,9 @@ public abstract class JobTask : IJsonSerializable
         _properties.Add(new Property<dynamic>("name", name));
     }
 
-    public string GetName()
-    {
-        Property<dynamic>? property = _properties.Find(tp => tp.Name == "name");
-
-        if (property == null)
-            throw new Exception("Name property not found");
-        
-        return property.Value;
-    }
-
     public bool UpdadeProperty(string name, dynamic value)
     {
-        Property<dynamic>? property = _properties.Find(tp => tp.Name == name);
+        Property<dynamic>? property = _properties.Find(prop => prop.Name == name);
 
         if (property == null)
             return false;
