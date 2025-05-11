558f29e1fd700a7eb90d3e50f1fbd7e6d108ca8c
Enorian <enorian.rajo@gmail.com>
2025-05-11 13:24:21 +0200

    Fix daily log generation for better abstraction

--- Easy-Save-Core/Logger/Logger.cs
+++ Easy-Save-Core/Logger/Logger.cs
@@ -19,9 +19,7 @@ public class Logger
     public string StatusLogPath { get => _statusLogPath; set => _statusLogPath = value; }
 
     private readonly ILogger _internalLogger;
-
-    private readonly List<JobTask> _jobTasks;
-
+    
     private static readonly Logger Instance = new Logger();
     
     private Logger()
@@ -36,8 +34,6 @@ public class Logger
             options.TimestampFormat = "[HH:mm:ss] ";
         }));
         _internalLogger = factory.CreateLogger(EasySaveCore.Name);
-
-        _jobTasks = [];
     }
 
     public static void Log(LogLevel level, string message)
@@ -50,35 +46,27 @@ public class Logger
     {
         return Instance;
     }
-
-    public void AddJob(JobTask task)
-    {
-        _jobTasks.Add(task);
-    }
     
-    public void SaveDailyLog(Format format)
+    public void SaveDailyLog(IJob job, List<JobTask> tasks, Format format)
     {
-        using (StreamWriter writer = new StreamWriter(GetDailyLogFilePath(), true))
+        using StreamWriter writer = new StreamWriter(GetDailyLogFilePath(), true);
+        string fileContent;
+        switch (format)
         {
-            string fileContent;
-            switch (format)
-            {
-                default:
-                case Format.Json:
-                    JsonObject json = JsonSerialize();
-                    fileContent = json.ToString();
-                    Log(LogLevel.Information, $"Saving daily log to file in JSON format at {GetDailyLogFilePath()}");
-                    break;
-                case Format.Xml:
-                    XmlElement xml = XmlSerialize();
-                    fileContent = xml.OuterXml;
-                    Log(LogLevel.Information, $"Saving daily log to file in XML format at {GetDailyLogFilePath()}");
-                    break;
-            }
-            writer.WriteLine(fileContent);
-            writer.WriteLine();
+            default:
+            case Format.Json:
+                JsonObject json = JsonSerialize(job, tasks);
+                fileContent = json.ToString();
+                Log(LogLevel.Information, $"Saving daily log to file in JSON format at {GetDailyLogFilePath()}");
+                break;
+            case Format.Xml:
+                XmlElement xml = XmlSerialize(job, tasks);
+                fileContent = xml.OuterXml;
+                Log(LogLevel.Information, $"Saving daily log to file in XML format at {GetDailyLogFilePath()}");
+                break;
         }
-        _jobTasks.Clear();
+        writer.WriteLine(fileContent);
+        writer.WriteLine();
     }
 
     private void LogToFile(StatusLogEntry logEntry)
@@ -96,36 +84,57 @@ public class Logger
         return Path.Combine(_dailyLogPath, $"dailyLog-{DateTime.Now:dd-MM-yyyy}.log");
     }
 
-    private JsonObject JsonSerialize()
+    private JsonObject JsonSerialize(IJob job, List<JobTask> tasks)
     {
         JsonObject json = new JsonObject();
 
-        foreach (JobTask jobTask in _jobTasks)
+        // Add job properties to JSON
+        foreach (Property<dynamic> prop in job.Properties)
+            json.Add(prop.Name, prop.Value);
+        
+        JsonArray array = new JsonArray();
+        
+        foreach (JobTask jobTask in tasks)
         {
+            JsonObject jsonTask = new JsonObject();
             foreach (Property<dynamic> property in jobTask.GetProperties())
-            {
-                json[property.Name] = property.Value?.ToString();
-            }
+                jsonTask.Add(property.Name, property.Value);
+            array.Add(jsonTask);
         }
+        
+        json.Add("tasks", array);
+        
         return json;
     }
 
-    private XmlElement XmlSerialize()
+    private XmlElement XmlSerialize(IJob job, List<JobTask> tasks)
     {
         XmlDocument doc = new XmlDocument();
-        XmlElement root = doc.CreateElement("LogEntries");
-
-        foreach (JobTask jobTask in _jobTasks)
+        XmlElement root = doc.CreateElement("root");
+        
+        // Add job properties to XML
+        foreach (Property<dynamic> prop in job.Properties)
         {
-            XmlElement entry = doc.CreateElement("LogEntry");
+            XmlElement propElement = doc.CreateElement(prop.Name);
+            propElement.InnerText = prop.Value.ToString() ?? string.Empty;
+            root.AppendChild(propElement);
+        }
+        
+        // Add tasks to XML
+        XmlElement tasksElement = doc.CreateElement("Tasks");
+
+        foreach (JobTask jobTask in tasks)
+        {
+            XmlElement entry = doc.CreateElement("Task");
             foreach (Property<dynamic> property in jobTask.GetProperties())
             {
                 XmlElement propElement = doc.CreateElement(property.Name);
-                propElement.InnerText = property.Value?.ToString() ?? string.Empty;
+                propElement.InnerText = property.Value.ToString() ?? string.Empty;
                 entry.AppendChild(propElement);
             }
-            root.AppendChild(entry);
+            tasksElement.AppendChild(entry);
         }
+        root.AppendChild(tasksElement);
 
         doc.AppendChild(root);
         return root;
@@ -134,7 +143,7 @@ public class Logger
 
 public class StatusLogEntry(LogLevel level, string message)
 {
-    public LogLevel Level { get; private set; } = level;
-    public string Message { get; private set; } = message;
-    public DateTime Timestamp { get; private set; } = DateTime.Now;
+    public LogLevel Level { get; } = level;
+    public string Message { get; } = message;
+    public DateTime Timestamp { get; } = DateTime.Now;
 }
