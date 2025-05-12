31cd3d9a7552eb6e0067cdab53f575ac9de112bb
CedricHoog <cedric.hoog@gmail.com>
2025-05-12 17:44:18 +0200

    update job menu name to be clearer

--- Easy-Save-CLI/EasySaveCli.cs
+++ Easy-Save-CLI/EasySaveCli.cs
@@ -306,17 +306,17 @@ public sealed class EasySaveCli : EasySaveView<BackupJob, ViewModelBackupJobBuil
         new SelectionPrompt<string>()
             .Title(L10N.GetTranslation("settings_menu.title"))
             .AddChoices(
-                L10N.GetTranslation("main_menu.change_language"), //TODO Rename main_menu -> option_menu
-                L10N.GetTranslation("main_menu.change_log_type").Replace("{LOGTYPE}", EasySaveCore.Utilities.Logger<BackupJob>.Get().DailyLogFormat.ToString()),
+                L10N.GetTranslation("settings_menu.change_language"), //TODO Rename main_menu -> option_menu
+                L10N.GetTranslation("settings_menu.change_log_type").Replace("{LOGTYPE}", EasySaveCore.Utilities.Logger<BackupJob>.Get().DailyLogFormat.ToString()),
                 L10N.GetTranslation("main.go_back")
             ));
 
-        if (choice == L10N.GetTranslation("main_menu.change_language"))
+        if (choice == L10N.GetTranslation("settings_menu.change_language"))
         {
             AddToMenuHistory(Menu.Language);
             DisplayLanguageMenu();
         }
-        else if (choice == L10N.GetTranslation("main_menu.change_log_type").Replace("{LOGTYPE}", EasySaveCore.Utilities.Logger<BackupJob>.Get().DailyLogFormat.ToString()))
+        else if (choice == L10N.GetTranslation("settings_menu.change_log_type").Replace("{LOGTYPE}", EasySaveCore.Utilities.Logger<BackupJob>.Get().DailyLogFormat.ToString()))
         {
             AddToMenuHistory(Menu.LogType);
             DisplayLogTypeMenu();
