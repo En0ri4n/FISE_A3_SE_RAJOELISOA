7898ad05a90ddc45c2efb156a7cdacdc18d76972
Enorian <enorian.rajo@gmail.com>
2025-05-14 23:18:41 +0200

    Add new languages
    Took 32 seconds

--- Easy-Save-Core/L10N/LangIdentifier.cs
+++ Easy-Save-Core/L10N/LangIdentifier.cs
@@ -9,9 +9,11 @@ public class LangIdentifier(string langId, string name)
 public static class Languages
 {
     public static readonly LangIdentifier EnUs = new LangIdentifier("en_us", "English (United States)");
-    public static readonly LangIdentifier FrFr = new("fr_fr", "Français (France)");
-    public static readonly LangIdentifier ZhCn = new("zh_cn", "中文 (简体中文 - 中国)");
+    public static readonly LangIdentifier FrFr = new LangIdentifier("fr_fr", "Français (France)");
+    public static readonly LangIdentifier IdId = new LangIdentifier("id_id", "Indonesia (Indonesia)");
+    public static readonly LangIdentifier MgMg = new LangIdentifier("mg_mg", "Malagasy (Madagascar)");
+    public static readonly LangIdentifier ZhCn = new LangIdentifier("zh_cn", "中文 (简体中文 - 中国)");
 
 
-    public static readonly List<LangIdentifier> SupportedLangs = [EnUs, FrFr, ZhCn];
+    public static readonly List<LangIdentifier> SupportedLangs = [EnUs, FrFr, IdId, MgMg, ZhCn];
 }
