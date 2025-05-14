f048a0e874a400a6ac87b2b304f594ed81ae1699
hungrywwaffle <78042192+hungrywwaffle@users.noreply.github.com>
2025-05-14 17:03:39 +0200

    Start interface skeleton
    Create windows and pages
    Place elements
    No binding yet

---
+++ Easy-Save-WPF/DeleteJob_Window.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Easy_Save_WPF
+{
+    /// <summary>
+    /// Logique d'interaction pour DeleteJob_Window.xaml
+    /// </summary>
+    public partial class DeleteJob_Window : Window
+    {
+        public DeleteJob_Window()
+        {
+            InitializeComponent();
+        }
+    }
+}
