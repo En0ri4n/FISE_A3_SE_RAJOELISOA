f0888e911f5d34a6db53daef86664d4c76387f1e
Enorian <enorian.rajo@gmail.com>
2025-05-25 20:44:43 +0200

    Add process name to process interrupted event
    Took 36 seconds
    
    Took 1 minute

--- Easy-Save-WPF/ManageJobs_Page.xaml.cs
+++ Easy-Save-WPF/ManageJobs_Page.xaml.cs
@@ -1,24 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Xml.Linq;
-using CLEA.EasySaveCore.Models;
-using CLEA.EasySaveCore.ViewModel;
-using Easy_Save_WPF;
 using EasySaveCore.Jobs.Backup.ViewModels;
 using EasySaveCore.Models;
-
 
 namespace Easy_Save_WPF
 {
@@ -35,15 +21,10 @@ namespace Easy_Save_WPF
             InitializeComponent();
             this.DataContext = BackupJobViewModel.Get().JobBuilder;
             this.jobsDatagrid.ItemsSource = BackupJobViewModel.Get().AvailableJobs;
-
-            // this.runBTN.IsEnabled = false;
-            // this.runBTN.Opacity = 0.5;
-            // this.pauseBTN.IsEnabled = false;
-            // this.pauseBTN.Opacity = 0.5;
-            // this.stopBTN.IsEnabled = false;
-            // this.stopBTN.Opacity = 0.5;
-            // this.deleteBTN.IsEnabled = false;
-            // this.deleteBTN.Opacity = 0.5;
+            BackupJobViewModel.Get().JobManager.JobInterruptedHandler += (job, processName) =>
+            {
+                MessageBox.Show($"Job {job.Name} and every following jobs have been interrupted by process {processName}.", "Job(s) Interruption(s)", MessageBoxButton.OK, MessageBoxImage.Error);
+            };
         }
         public void QuitBTN_Click(object sender, RoutedEventArgs e)
         {
