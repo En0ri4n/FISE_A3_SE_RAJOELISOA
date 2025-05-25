0334c982b1dcdbeeca91ff311a401801abd3d217
Anthony <schneppanthony@gmail.com>
2025-05-25 23:19:18 +0200

    Added button to open config file inside the Options Configuration Popup

--- Easy-Save-WPF/OptionsPopup.xaml.cs
+++ Easy-Save-WPF/OptionsPopup.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
 using EasySaveCore.Jobs.Backup.ViewModels;
 
 namespace Easy_Save_WPF
@@ -13,5 +14,16 @@ namespace Easy_Save_WPF
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e) => Close();
+
+        public void configFileBTN_Click(object sender, RoutedEventArgs e)
+        {
+            var path = "config.json";
+
+            using Process myProcess = new Process();
+            myProcess.StartInfo.FileName = path;
+            myProcess.StartInfo.Verb = "open";
+            myProcess.StartInfo.UseShellExecute = true;
+            myProcess.Start();
+        }
     }
 }
