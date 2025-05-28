9237466198123445aed8a582117768c931f898ce
Anthony <schneppanthony@gmail.com>
2025-05-28 12:02:42 +0200

    Change Version to 3.0.0

--- Easy-Save-Core/EasySaveCore.cs
+++ Easy-Save-Core/EasySaveCore.cs
@@ -13,7 +13,7 @@ namespace CLEA.EasySaveCore.Core
     public class EasySaveCore
     {
         public const string Name = "EasySave-CLEA";
-        public static readonly Version Version = new Version(2, 0, 0);
+        public static readonly Version Version = new Version(3, 0, 0);
 
         private static EasySaveCore _instance;
         
