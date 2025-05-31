e3a8a1cc4a8d845d371f80cf51ac06e148e895ee
Enorian <enorian.rajo@gmail.com>
2025-05-31 12:57:56 +0200

    Cleanup
    Took 16 minutes

--- Easy-Save-Remote/ViewModel/ClientViewModel.cs
+++ Easy-Save-Remote/ViewModel/ClientViewModel.cs
@@ -1,12 +1,28 @@
-﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using EasySaveRemote;
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using EasySaveRemote.Client.Commands;
 using EasySaveRemote.Client.DataStructures;
+using Newtonsoft.Json;
 
-namespace Easy_Save_Remote.ViewModel
+namespace EasySaveRemote.Client.ViewModel
 {
     public class ClientViewModel
     {
+        public ClientBackupJobBuilder BackupJobBuilder { get; } = new ClientBackupJobBuilder();
         public ObservableCollection<ClientBackupJob> AvailableBackupJobs => RemoteClient.Get().BackupJobs;
+        public ICommand BuildJobCommand { get; set; }
+        
+        public void InitializeCommands()
+        {
+            BuildJobCommand = new RelayCommand(isCreation =>
+            {
+                if(!bool.TryParse(isCreation?.ToString(), out bool isJobCreation))
+                    return;
+                
+                ClientBackupJob clientBackupJob = RemoteClient.Get().ViewModel.BackupJobBuilder.Build();
+                RemoteClient.Get().NetworkClient.SendMessage(NetworkMessage.Create(isJobCreation ? MessageType.BackupJobAdd : MessageType.BackupJobUpdate, 
+                    JsonConvert.SerializeObject(clientBackupJob)));
+            }, _ => true);
+        }
     }
 }
